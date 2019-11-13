46f459363623829723f831f05ff3b5cca4833857
andrei-kaliada <koleda05@gmail.com>
2019-11-13 21:43:33 +0100

    Refactoring code

--- chat/src/components/Message/index.js
+++ chat/src/components/Message/index.js
@@ -3,28 +3,55 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { ru } from 'date-fns/locale';
+import { Icon } from 'antd';
 
 import './Message.scss';
 
-const Message = ({ avatar, text , date, user, isMe}) => {
+const iconRead = {
+    display: 'flex',
+    alignItems: 'flex-end',
+    color: 'red',
+    marginBottom: '20px',
+    marginRight: '8px',
+}
 
-    return(
-        <div className={classNames('message', {'message--isme':isMe})}>
-            <div className="message__avatar">
-                <img src={avatar} alt={`Avatar ${user.fullname}`} />
-            </div>
+const Message = ({ avatar, text, date, user, isMe, isReady, attachments }) => {
+
+    return (
+        <div className={classNames('message', { 'message--isme': isMe })}>
             <div className="message__content">
-                <div className="message__bubble">
-                    <div className="message__text">
-                        <p>{text}</p>
+                {isMe ?
+                    <div style={iconRead}>
+                        {isReady ?
+                            (<Icon type="check" />) :
+                            (<Icon type="check-circle" style={{ color: 'green' }} />)
+                        }
                     </div>
-                </div>
-                <span className="message__date">
-                        {formatDistanceToNow(new Date(2016, 7, 1),  {addSuffix: true, locale: ru })}
-                </span>
+                    : ""}
+                    <div className="message__avatar">
+                        <img src={avatar} alt={`Avatar ${user.fullname}`} />
+                    </div>
+                    <div className="message__info">
+                        <div className="message__bubble">
+                            <div className="message__text">
+                                <p>{text}</p>
+                            </div>
+                        </div>
+                        <span className="message__date">
+                            {formatDistanceToNow(new Date(date), { addSuffix: true, locale: ru })}
+                        </span>
+                    </div> 
             </div>
-            
+
+            {/* <div className="message__attachments">
+                {attachments && attachments.map(item => (
+                    <div className="message__attachments-item">
+                        <img src={item.url} alt={item.filename} />
+                    </div>
+                ))}
+            </div> */}
         </div>
+
     );
 }
 
@@ -33,10 +60,10 @@ Message.defaultProps = {
 }
 
 Message.propTypes = {
-    avatar:PropTypes.string,
-    text:PropTypes.string,
-    date:PropTypes.string,
-    user:PropTypes.object,
+    avatar: PropTypes.string,
+    text: PropTypes.string,
+    date: PropTypes.string,
+    user: PropTypes.object,
 }
 
 export default Message;
--- chat/src/pages/Home/index.jsx
+++ chat/src/pages/Home/index.jsx
@@ -9,11 +9,44 @@ const Home = () => {
         <section className="home">
             <Message 
             avatar="https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1" 
-            text="Hello world" 
+            text="Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello world" 
+            date="Mon Nov 11 2019 21:20:24"
+           
+            />
+            <Message 
+            avatar="https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1" 
+            text="Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello world" 
+            date="Mon Nov 11 2019 21:20:24"
+            
+            />
+            <Message 
+            avatar="https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1" 
+            text="Hello worldHello worldHello worldHello worldHello worldHello worldHello world" 
             date="Mon Nov 11 2019 21:20:24"
             isMe={true}
+            isReady={false}
+            attachments={[
+                {
+                    filename:"image.jpg",
+                    url:"https://source.unsplash.com/100x100/?random=1&nature,water/"
+                },
+                {
+                    filename:"image.jpg",
+                    url:"https://source.unsplash.com/100x100/?random=2&nature,water/"
+                },
+                {
+                    filename:"image.jpg",
+                    url:"https://source.unsplash.com/100x100/?random=3&nature,water/"
+                },
+            ]}
             />
-           
+            <Message 
+            avatar="https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1" 
+            text="Hello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello worldHello world" 
+            date="Mon Nov 11 2019 21:20:24"
+            isMe={true}
+            isReady={true}
+            />  
         </section>
     );
 }
