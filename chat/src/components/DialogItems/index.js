b1bcaee05d859374dd4abc94352555a42cdccf65
andrei-kaliada <koleda05@gmail.com>
2020-01-08 16:34:37 +0100

    Added style to dialogs-avatar

--- chat/src/components/DialogItems/index.js
+++ chat/src/components/DialogItems/index.js
@@ -4,28 +4,43 @@ import { Time, IconRead } from '../index';
 
 import './DialogItems.scss';
 
-const Block = ({ user, message, date }) => {
+const getAvatar = avatar => {
+    if(avatar){
+        return (
+            <img src={avatar} alt=""/>
+        );
+    }else{
+        //make avatar
+    }
+}
+
+const DialogItem = ({ user, message, date }) => {
 
     return(
-       <div className="dialogs__item">
+       <div className="dialogs__item dialogs__item--online">
            <div className="dialogs__item-avatar">
                {/* <img src={user.avatar} alt={`${user.fullname}`} /> */}
-               <img src='https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1' />
+               {getAvatar('https://sun9-58.userapi.com/c854220/v854220990/11a61d/M6aR0K7W21k.jpg?ava=1')}
+              
            </div>
            <div className="dialogs__item-info">
                 <div className="dialogs__item-info-top">
                     <b>Andrey Koleda</b>
                     <span>
-                        <Time date={date}/>
+                        {/* <Time date={date}/> */}
+                        13.03
                     </span>
                 </div>
                 <div className="dialogs__item-info-bottom">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                   <IconRead isMe={true} isReady={false} />
+                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                   {/* <IconRead isMe={true} isReady={false} /> */}
+                   <div className="dialogs__item-info-bottom-count">
+                       3
+                   </div>
                 </div>
            </div>
        </div>
     );
 }
 
-export default Block;
+export default DialogItem;
